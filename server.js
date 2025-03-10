96c83a393f06b5adbc25eab5dba4595b30eb9203
Dominick Woodward <37197437+v2dom@users.noreply.github.com>
2025-03-10 14:20:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.post("/api/spotify/token", async (req, res) => {
     const { code } = req.body;
 
     const clientId = "c0bf7f17b46b4433b09d1eda0f48af69";
-    const clientSecret = "27c98fdbd46d41f38ea0eb10a0cdfae1";
+    const clientSecret = "hidden";
     const redirectUri = "https://v2dom.dev/clarify/home";
     const scope = "user-top-read user-library-read";
 
